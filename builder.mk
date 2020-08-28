c5afe94b4e23cc37295eff31b51311ac6e86d3e3
Bastien Chatelard <chatel.bast@gmail.com>
2020-08-28 18:43:49 +0200

    Publish the dokcer description

--- builder.mk
+++ builder.mk
@@ -12,3 +12,9 @@ publish:
 		docker tag koyeb/$(NAME) koyeb/$(NAME):$$tag ; \
 		docker push koyeb/$(NAME):$$tag ; \
 	done ;\
+	docker run -v $PWD:/workspace \
+		-e DOCKERHUB_USERNAME='${DOCKERHUB_USERNAME}' \
+		-e DOCKERHUB_PASSWORD='${DOCKERHUB_PASSWORD}' \
+		-e DOCKERHUB_REPOSITORY='koyeb/${NAME}' \
+		-e README_FILEPATH='/workspace/README.md' \
+		peterevans/dockerhub-description:2.3
