49c9d353c4149be3011d0a21d76fb75a6294e9ac
Bastien Chatelard <chatel.bast@gmail.com>
2020-08-28 17:40:42 +0200

    Update builder.mk

--- builder.mk
+++ builder.mk
@@ -6,8 +6,8 @@ build:
 	$(SCRIPT_DIR)/package.sh $(NAME)
 
 publish:
-	echo docker push koyeb/$(NAME)
+	docker push koyeb/$(NAME)
 	for tag in $(TAGS) ; \
 	do \
-		echo docker push koyeb/$(NAME):$$tag ; \
+		docker push koyeb/$(NAME):$$tag ; \
 	done ;\
