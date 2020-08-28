caadac575c9d9b879134cba6c86c0ac4a420f506
Bastien Chatelard <chatel.bast@gmail.com>
2020-08-28 18:49:12 +0200

    Fix invalid variable

--- builder.mk
+++ builder.mk
@@ -11,8 +11,9 @@ publish:
 	do \
 		docker tag koyeb/$(NAME) koyeb/$(NAME):$$tag ; \
 		docker push koyeb/$(NAME):$$tag ; \
-	done ;\
-	docker run -v $PWD:/workspace \
+	done ;
+
+	docker run -v ${PWD}:/workspace \
 		-e DOCKERHUB_USERNAME='${DOCKER_USERNAME}' \
 		-e DOCKERHUB_PASSWORD='${DOCKER_PASSWORD}' \
 		-e DOCKERHUB_REPOSITORY='koyeb/${NAME}' \
