bac0f8981cc820ef1c21115dbb039e5ca82cb84e
Bastien Chatelard <chatel.bast@gmail.com>
2020-08-31 09:27:56 +0200

    Add github actions

--- package.sh
+++ package.sh
@@ -3,8 +3,8 @@
 set -x
 set -e
 
-NAME=$1
 SCRIPT_DIR=${SCRIPT_DIR:=.}
+DOCKER_ORGANIZATION=${DOCKER_ORGANIZATION:=koyeb}
 
 name=$(cat koyeb.yaml | yq r - functions[0].name)
 runtime=$(cat koyeb.yaml | yq r - functions[0].runtime)
@@ -13,5 +13,5 @@ handler=$(cat koyeb.yaml | yq r - functions[0].handler)
 mkdir -p $SCRIPT_DIR/out
 mkdir -p $SCRIPT_DIR/.cache
 
-docker run --rm -v $SCRIPT_DIR/build.sh:/build.sh -v $PWD:/koyeb/repo/git -v $SCRIPT_DIR/.cache:/var/buildpack/cache -e XDG_CACHE_HOME=/var/buildpack/cache -e KOYEB_OUTPUT_FILE=/koyeb/build/$NAME.tar -v $SCRIPT_DIR/out:/koyeb/build koyeb/runtime:build-$runtime /build.sh /koyeb/repo/git /var/buildpack/cache
-docker build --build-arg HANDLER=$handler --build-arg RUNTIME=$runtime --build-arg PROJECT=$NAME -t koyeb/$NAME $SCRIPT_DIR
+docker run --rm -v $SCRIPT_DIR/build.sh:/build.sh -v $PWD:/koyeb/repo/git -v $SCRIPT_DIR/.cache:/var/buildpack/cache -e XDG_CACHE_HOME=/var/buildpack/cache -e KOYEB_OUTPUT_FILE=/koyeb/build/$name.tar -v $SCRIPT_DIR/out:/koyeb/build koyeb/runtime:build-$runtime /build.sh /koyeb/repo/git /var/buildpack/cache
+docker build --build-arg HANDLER=$handler --build-arg RUNTIME=$runtime --build-arg PROJECT=$name -t $DOCKER_ORGANIZATION/$name $SCRIPT_DIR
---
+++ publish.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+set -e
+
+SCRIPT_DIR=${SCRIPT_DIR:=.}
+DOCKER_ORGANIZATION=${DOCKER_ORGANIZATION:=koyeb}
+TAGS=""
+NAME=$(cat koyeb.yaml | yq r - functions[0].name)
+
+if [[ "$GITHUB_REF" =~ ^refs/tags/v.*$ ]]; then
+  release=${GITHUB_REF#refs/*/}
+  major=$(echo $release | awk -F \. {'print $1'})
+  minor=$(echo $release | awk -F \. {'print $1 "." $2'})
+  TAGS="$release"
+  TAGS+="$major"
+  TAGS+="$minor"
+fi
+
+IMAGE=$DOCKER_ORGANIZATION/$NAME
+
+# Push image and tags
+docker push $IMAGE
+for tag in $TAGS ;
+do
+  docker tag $IMAGE $IMAGE:$tag
+  docker push $IMAGE:$tag
+done
+
+# Update docker desc with readme
+# Not working with access token, see https://github.com/docker/hub-feedback/issues/1927
+#docker run -v ${PWD}:/workspace \
+#  -e DOCKERHUB_USERNAME='$DOCKER_USERNAME' \
+#  -e DOCKERHUB_PASSWORD='$DOCKER_PASSWORD' \
+#  -e DOCKERHUB_REPOSITORY='$IMAGE' \
+#  -e README_FILEPATH='/workspace/README.md' \
+#  peterevans/dockerhub-description:2.3
