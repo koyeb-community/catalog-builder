7a60455f52ac4e0b134cbb9184fa5172b8c3fe7d
Bastien Chatelard <bchatelard@users.noreply.github.com>
2020-09-23 15:49:49 +0200

    Update build image entrypoint

--- package.sh
+++ package.sh
@@ -13,5 +13,5 @@ handler=$(cat koyeb.yaml | yq r - functions[0].handler)
 mkdir -p $SCRIPT_DIR/out
 mkdir -p $SCRIPT_DIR/.cache
 
-docker run --rm -v $SCRIPT_DIR/build.sh:/build.sh -v $PWD:/koyeb/repo/git -v $SCRIPT_DIR/.cache:/var/buildpack/cache -e XDG_CACHE_HOME=/var/buildpack/cache -e KOYEB_OUTPUT_FILE=/koyeb/build/$name.tar -v $SCRIPT_DIR/out:/koyeb/build koyeb/runtime:build-$runtime /build.sh /koyeb/repo/git /var/buildpack/cache
+docker run --rm -v $SCRIPT_DIR/build.sh:/build.sh -v $PWD:/koyeb/repo/git -v $SCRIPT_DIR/.cache:/var/buildpack/cache -e XDG_CACHE_HOME=/var/buildpack/cache -e KOYEB_OUTPUT_FILE=/koyeb/build/$name.tar -v $SCRIPT_DIR/out:/koyeb/build --entrypoint /build.sh koyeb/runtime:build-$runtime /koyeb/repo/git /var/buildpack/cache
 docker build --build-arg HANDLER=$handler --build-arg RUNTIME=$runtime --build-arg PROJECT=$name -t $DOCKER_ORGANIZATION/$name $SCRIPT_DIR
